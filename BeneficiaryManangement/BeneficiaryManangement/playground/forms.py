2bed4f9e2feea31ea7dda9c8a2ca44980e53f7c9
Apfeh Rams <140980216+Apfeh@users.noreply.github.com>
2025-05-12 11:08:35 +0200

    Delete BeneficiaryManangement directory

--- BeneficiaryManangement/BeneficiaryManangement/playground/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Beneficiary
-
-class BeneficiaryForm(forms.ModelForm):
-    class Meta:
-        model = Beneficiary
-        fields = ['name', 'contactNumber', 'relationshipToInsured']
--- BeneficiaryManangement/BeneficiaryManangement/playground/url.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('beneficiaries/', views.beneficiary_list, name='beneficiary_list'),
-    path('beneficiaries/add/', views.beneficiary_create, name='beneficiary_create'),
-    path('beneficiaries/edit/<int:id>/', views.beneficiary_update, name='beneficiary_update'),
-    path('beneficiaries/delete/<int:id>/', views.beneficiary_delete, name='beneficiary_delete'),
-]
