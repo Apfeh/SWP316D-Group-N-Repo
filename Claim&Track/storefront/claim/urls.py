a54cd35dad43300e7df7d5ee17e016c5cd787c30
Apfeh Rams <140980216+Apfeh@users.noreply.github.com>
2025-05-12 11:12:40 +0200

    Delete Claim&Track/storefront directory

--- Claim&Track/storefront/claim/urls.py
+++
@@ -1,7 +0,0 @@
-# claim/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.claim, name='claim_dashboard'),  # This makes claim_dashboard the default route
-]
